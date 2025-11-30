031cf381e78d14010c4d9913257d4a6f53565da9
Scott Thies <scott@thies.tech>
2025-11-30 11:23:21 -0500

    feat: Configure Firebase and shadcn-ui

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+// To avoid re-initializing on hot reloads in dev, check if it's already been initialized.
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Initialize Firebase services
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
